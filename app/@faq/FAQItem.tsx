8be4fc1bfa88e5e57c239e6bc0711892c1e64e24
mozix5 <mosinmd827@gmail.com>
2024-07-23 02:48:47 +0530

    add accordian

--- app/@faq/FAQItem.tsx
+++ app/@faq/FAQItem.tsx
@@ -1,29 +1,30 @@
 "use client";
 import React, { useState } from "react";
-import Image from "next/image";
-import add from "@/public/svg/add_36.svg";
-import cross from "@/public/svg/cross.svg";
-import IconLabel from "../components/IconLabel";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 type FAQItemProps = {
   question: string;
   answer: string;
+  value: string;
 };
 
-const FAQItem = ({ question, answer }: FAQItemProps) => {
+const FAQItem = ({ question, answer, value }: FAQItemProps) => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div className="border-b border-gray-200 py-6">
-      <div
-        className="flex justify-between items-center cursor-pointer "
-        onClick={() => setIsOpen(!isOpen)}
-      >
-        <div className=" font-semibold">{question}</div>
-        {isOpen ? <IconLabel icon={cross} /> : <IconLabel icon={add} />}
-      </div>
-      {isOpen && <div className="pb-2 text-gray-700 mt-6">{answer}</div>}
-    </div>
+    <Accordion type="single" collapsible>
+      <AccordionItem value={value}>
+        <AccordionTrigger className=" font-semibold text-lg">
+          {question}
+        </AccordionTrigger>
+        <AccordionContent>{answer}</AccordionContent>
+      </AccordionItem>
+    </Accordion>
   );
 };
 
