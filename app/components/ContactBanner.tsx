0b1c49e3d611667632f0ae7f2db6c92dbdb311af
mozix5 <mosinmd827@gmail.com>
2024-07-23 01:46:43 +0530

    fix

--- app/components/ContactBanner.tsx
+++ app/components/ContactBanner.tsx
@@ -6,11 +6,11 @@ const ContactBanner = () => {
     <div className="px-20 bg-gradient-to-r from-yellow to-moss-green flex py-12">
       <div className=" text-white space-y-8 w-1/2">
         <div>
-          <h1>Let's discuss</h1>
+          <h1>Let&apos;s discuss</h1>
           <h1>your project</h1>
         </div>
         <p className="mr-32">
-          Let's figure out how to create an effective application, its cost and
+          Let&apos;s figure out how to create an effective application, its cost and
           terms of its development
         </p>
       </div>
